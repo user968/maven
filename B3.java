a2ad8dd58625fec01948689dc452a6e28bc50692
User968 <kvamshi968@gmail.com>
2017-09-01 18:50:55 -0500

    Add existing files

---
+++ B3.java
@@ -0,0 +1,12 @@
+package pack1;
+public class B3 
+{
+	public static void test1()
+	{
+		System.out.println("from test1()");
+		System.out.println("from test11()");
+		System.out.println("from test111()");
+	}
+	
+}
+
